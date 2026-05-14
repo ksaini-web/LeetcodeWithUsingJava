67305bca66c71798154a8a6ec3b163b7d748d9ad
ksaini-web <kartiksaini9084@gmail.com>
2026-05-14 13:32:29 +0530

    Create leetcode50.java

---
+++ leetcode50.java
@@ -0,0 +1,34 @@
+class Solution {
+    public double myPow(double x, int n) {
+        
+        if(n==0) return 1.0;
+        if(x==0) return 0;
+        if(x==1) return 1.0;
+        if(x == -1 && n%2 == 0) return 1.0;
+        if(x == -1 && n%2 != 0) return -1.0;
+
+        long binary = n ; 
+
+        double ans = 1;
+
+        if(n<0){
+             x= 1/x;
+            binary = -n;
+        }
+
+
+        while(binary>0){
+
+            if(binary%2 == 1){
+
+                ans*=x;
+
+            }
+            x*=x;
+            binary/=2;
+
+        }
+
+        return ans ;
+    } 
+}
