38ab676dc3555149844b131c1af0f57ecd83d7cf
ksaini-web <kartiksaini9084@gmail.com>
2026-06-08 23:07:55 +0530

    Implement pivotArray method to rearrange elements

---
+++ Leetcode2161.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int idx = 0;
+
+        // Elements less than pivot
+        for (int num : nums) {
+            if (num < pivot) {
+                result[idx++] = num;
+            }
+        }
+
+        // Elements equal to pivot
+        for (int num : nums) {
+            if (num == pivot) {
+                result[idx++] = num;
+            }
+        }
+
+        // Elements greater than pivot
+        for (int num : nums) {
+            if (num > pivot) {
+                result[idx++] = num;
+            }
+        }
+
+        return result;
+    }
+}
