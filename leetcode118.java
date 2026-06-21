bb5dea0c8e7a02b68003e998d321d574010c42e9
ksaini-web <kartiksaini9084@gmail.com>
2026-06-21 15:58:20 +0530

    Implement Pascal's Triangle generation

---
+++ leetcode118.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> Ans = new ArrayList<>();
+
+        for(int i =0 ; i< numRows ;i++){
+
+            List<Integer> row =new ArrayList<>();
+
+            for(int j = 0;j<=i ;j++){
+
+                if(j==0 || j ==i){
+
+                    row.add(1);
+
+
+                }else {
+
+                    row.add(Ans.get(i-1).get(j-1) + Ans.get(i-1).get(j));
+                }
+
+
+            }
+
+            Ans.add(row);
+        }
+
+        return Ans;
+        
+    }
+}
