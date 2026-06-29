2d4b8232ea3d3e1dfaf62166c0167b0900624f76
ksaini-web <kartiksaini9084@gmail.com>
2026-06-29 17:39:09 +0530

    Implement lengthOfLastWord method in Solution class

---
+++ leetcode58.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        int count = 0 ;
+
+        s=s.trim();
+
+        for(int i = s.length()-1;i>=0;i--){
+
+            char ch = s.charAt(i);
+
+            if(ch == ' '){
+                return count;
+            }else{
+                count++;
+            }
+        }
+
+        return count;
+        
+    }
+}
