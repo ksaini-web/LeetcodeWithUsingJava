cefcc75211c7e0d5e849bd37261e825c4a4a5e60
ksaini-web <kartiksaini9084@gmail.com>
2026-07-06 15:27:26 +0530

    Implement Reverse Polish Notation evaluator

---
+++ leetcode150.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+
+        Stack<Integer> st = new Stack<>();
+
+        for (String token : tokens) {
+
+            switch (token) {
+
+                case "+":
+                    st.push(st.pop() + st.pop());
+                    break;
+
+                case "-": {
+                    int b = st.pop();
+                    int a = st.pop();
+                    st.push(a - b);
+                    break;
+                }
+
+                case "*":
+                    st.push(st.pop() * st.pop());
+                    break;
+
+                case "/": {
+                    int b = st.pop();
+                    int a = st.pop();
+                    st.push(a / b);
+                    break;
+                }
+
+                default:
+                    st.push(Integer.parseInt(token));
+            }
+        }
+
+        return st.pop();
+    }
+}
