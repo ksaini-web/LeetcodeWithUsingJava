5006577627f541fd5c4037aede65b27286e5ecbf
ksaini-web <kartiksaini9084@gmail.com>
2026-07-31 20:11:17 +0530

    Create leetcode28.java

---
+++ leetcode28.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int strStr(String haystack, String needle) { 
+
+
+     for(int i = 0; i<=haystack.length()-needle.length() ;i++){
+
+        if(haystack.charAt(i) == needle.charAt(0)){
+
+            int j =0;
+
+          while(j<needle.length() && haystack.charAt(i+j) == needle.charAt(j)){
+            
+            j++;
+
+          }
+
+          if(j == needle.length()){
+
+            return i;
+          }
+
+        }
+
+       
+
+        
+     }
+
+ return -1;
+        
+    }
+}
