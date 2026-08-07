a0f04572995167010d70c5c747ff6e14ebbe576f
ksaini-web <kartiksaini9084@gmail.com>
2026-08-07 23:44:23 +0530

    Implement minimum absolute difference solution

---
+++ leetcode1200.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+
+        Arrays.sort(arr);
+
+        int minDiff = Integer.MAX_VALUE;
+
+        // Find minimum difference
+        for (int i = 1; i < arr.length; i++) {
+            minDiff = Math.min(minDiff, arr[i] - arr[i - 1]);
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        // Collect pairs
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] == minDiff) {
+                ans.add(Arrays.asList(arr[i - 1], arr[i]));
+            }
+        }
+
+        return ans;
+    }
+}
