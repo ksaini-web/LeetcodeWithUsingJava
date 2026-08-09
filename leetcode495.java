33616db74f3db68b4e7e58b3488629fca4b0edb5
ksaini-web <kartiksaini9084@gmail.com>
2026-08-09 23:31:47 +0530

    Create leetcode495.java

---
+++ leetcode495.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+
+        int total = 0;
+
+        for (int i = 0; i < timeSeries.length - 1; i++) {
+            total += Math.min(duration, timeSeries[i + 1] - timeSeries[i]);
+        }
+
+        // Last attack always contributes full duration
+        total += duration;
+
+        return total;
+    }
+}
