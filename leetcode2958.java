b0635ad53550c833970e3e9e42da7d1099d9921e
ksaini-web <kartiksaini9084@gmail.com>
2026-08-12 18:04:23 +0530

    Implement maxSubarrayLength method in Solution class

---
+++ leetcode2958.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+        
+        HashMap<Integer ,Integer> map = new HashMap<>();
+
+        int left = 0 , ans =0  ;
+        
+
+        for(int i = 0;i<nums.length ;i++){
+
+            
+         
+            map.put(nums[i] ,map.getOrDefault(nums[i],0)+1);
+
+
+
+            while(map.get(nums[i]) > k){
+
+
+                map.put(nums[left] , map.get(nums[left]) -1);
+                left++;
+
+            }
+
+            ans = Math.max(ans,i-left +1);
+           
+
+
+            
+
+            
+        } 
+
+        return ans;
+    }
+}
